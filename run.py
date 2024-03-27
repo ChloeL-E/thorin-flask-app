5e87d23db56bde99ab2f75f6c6ae77411c4dc1d6
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-03-27 21:58:29 +0000

    Ammending code in run.py, base.html

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
@@ -15,25 +16,27 @@ def index():
 
 @app.route("/about")
 def about():
-    data=[]
+    data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
-    with open("data/company.json", "r") as json.data:
-        data = json.load(json.data)
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html", member=member) 
+    return render_template("member.html", member=member)
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have recieved your message!".format(
+        flash("Thanks {}, we have received your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
@@ -41,6 +44,7 @@ def contact():
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
+
 
 if __name__ == "__main__":
     app.run(
